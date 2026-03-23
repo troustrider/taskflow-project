e572ccfabd9cbb0351faf85e52bcf1a1c079545b
Karim Abatouy <akarim1398@gmail.com>
2026-03-23 11:15:32 +0100

    fix: unifica las rutas api de Vercel en una sola function

--- api/docs.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-const app = require("../server/src/index");
-
-module.exports = (req, res) => {
-  req.url = "/api/docs/";
-  return app(req, res);
-};
--- api/router.js
+++ api/router.js
@@ -1,9 +1,19 @@
 "use strict";
 
-const app = require("../../../server/src/index");
+const app = require("../server/src/index");
+
+function getPathValue(query) {
+  const value = query?.path;
+
+  if (Array.isArray(value)) {
+    return value[0] || "";
+  }
+
+  return typeof value === "string" ? value : "";
+}
 
 module.exports = (req, res) => {
-  const { id, ...rest } = req.query || {};
+  const { path, ...rest } = req.query || {};
   const searchParams = new URLSearchParams();
 
   for (const [key, value] of Object.entries(rest)) {
@@ -14,8 +24,7 @@ module.exports = (req, res) => {
     }
   }
 
-  const taskId = Array.isArray(id) ? id[0] : id;
-  const pathname = `/api/v1/tasks/${taskId}`;
+  const pathname = `/api/${getPathValue({ path })}`;
   const search = searchParams.toString();
 
   req.url = search ? `${pathname}?${search}` : pathname;
